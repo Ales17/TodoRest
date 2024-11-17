2c5579b6792413282b8713967dba576a5001234d
Ales Bacul <alesbacul@gmail.com>
2024-11-17 16:05:00 +0100

    create task repository

--- src/main/java/cz/ales17/test/repository/TaskRepository.java
+++ src/main/java/cz/ales17/test/repository/TaskRepository.java
@@ -1,9 +1,16 @@
 package cz.ales17.test.repository;
 
+import cz.ales17.test.entity.Company;
 import cz.ales17.test.entity.Task;
+import cz.ales17.test.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findAllByCreatedByIs(UserEntity user);
+
+    List<Task> findAllByCreatedBy_Company(Company company);
 }
