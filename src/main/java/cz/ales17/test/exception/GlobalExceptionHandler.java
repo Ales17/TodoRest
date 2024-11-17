8a17834f4be4342d90cd1c31e1ac5294e0a5358f
Ales Bacul <alesbacul@gmail.com>
2024-11-17 16:30:46 +0100

    create exception handler

---
+++ src/main/java/cz/ales17/test/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package cz.ales17.test.exception;
+
+import cz.ales17.test.entity.ErrorObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestHeaderException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorObject> handleUserNotFoundException(UserNotFoundException e) {
+        ErrorObject eo = new ErrorObject(HttpStatus.NOT_FOUND.value(), e.getMessage(), new Date());
+        return new ResponseEntity<>(eo, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErrorObject> handleMissingRequestHeaderException(MissingRequestHeaderException e) {
+        ErrorObject eo = new ErrorObject(HttpStatus.BAD_REQUEST.value(), e.getMessage(), new Date());
+        return new ResponseEntity<>(eo, HttpStatus.BAD_REQUEST);
+    }
+}
