a48444dcc9239e776f58f71983b75871a6b2a501
Ales Bacul <alesbacul@gmail.com>
2024-11-17 11:04:53 +0100

    add company repository to the project

---
+++ src/main/java/cz/ales17/test/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package cz.ales17.test.repository;
+
+import cz.ales17.test.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
