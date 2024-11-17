9b22e8ded5a43180255b8b612402b963c8b547d3
Ales Bacul <alesbacul@gmail.com>
2024-11-17 16:06:45 +0100

    add not found handling to user service

--- src/main/java/cz/ales17/test/service/UserService.java
+++ src/main/java/cz/ales17/test/service/UserService.java
@@ -1,22 +1,25 @@
 package cz.ales17.test.service;
 
 import cz.ales17.test.entity.UserEntity;
+import cz.ales17.test.exception.UserNotFoundException;
 import cz.ales17.test.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<UserEntity> findAll() {
         return userRepository.findAll();
     }
 
     public UserEntity findOne(Long id) {
-        return userRepository.findById(id).orElse(null);
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User not found"));
     }
 }
