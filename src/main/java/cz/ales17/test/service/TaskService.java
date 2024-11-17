3fe1270e6a7bfd0569127c516a5c99cee33bded5
Ales Bacul <alesbacul@gmail.com>
2024-11-17 16:06:17 +0100

    add task filtering methods

--- src/main/java/cz/ales17/test/service/TaskService.java
+++ src/main/java/cz/ales17/test/service/TaskService.java
@@ -1,8 +1,10 @@
 package cz.ales17.test.service;
 
+import cz.ales17.test.entity.Company;
+import cz.ales17.test.entity.Role;
 import cz.ales17.test.entity.Task;
+import cz.ales17.test.entity.UserEntity;
 import cz.ales17.test.repository.TaskRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,12 +12,33 @@ import java.util.List;
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final UserService userService;
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, UserService userService) {
         this.taskRepository = taskRepository;
+        this.userService = userService;
     }
 
     public List<Task> findAll() {
         return taskRepository.findAll();
+    }
+
+    public List<Task> findByUser(UserEntity user) {
+        return taskRepository.findAllByCreatedByIs(user);
+    }
+
+    public List<Task> findByCompany(Company company) {
+        return taskRepository.findAllByCreatedBy_Company(company);
+    }
+
+    public List<Task> findTasksByUserRole(Long userId) {
+        UserEntity user = userService.findOne(userId);
+        Role userRole = user.getRole();
+
+        return switch (userRole) {
+            case USER -> findByUser(user);
+            case COMPANY_ADMIN -> findByCompany(user.getCompany());
+            case SUPER_USER -> findAll();
+        };
     }
 }
