1682b9369a2d685e6ab81bbe6baaf97b63483ca9
Ales Bacul <alesbacul@gmail.com>
2024-11-18 21:49:27 +0100

    add task create method to controller

--- src/main/java/cz/ales17/test/controller/TestController.java
+++ src/main/java/cz/ales17/test/controller/TestController.java
@@ -1,7 +1,9 @@
 package cz.ales17.test.controller;
 
+import cz.ales17.test.entity.Company;
 import cz.ales17.test.entity.Task;
 import cz.ales17.test.entity.UserEntity;
+import cz.ales17.test.repository.CompanyRepository;
 import cz.ales17.test.service.TaskService;
 import cz.ales17.test.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,12 @@ public class TestController {
 
     private final UserService userService;
     private final TaskService taskService;
+    private final CompanyRepository companyRepository;
 
-    public TestController(UserService userService, TaskService taskService) {
+    public TestController(UserService userService, TaskService taskService, CompanyRepository companyRepository) {
         this.userService = userService;
         this.taskService = taskService;
+        this.companyRepository = companyRepository;
     }
 
     @GetMapping("/")
@@ -39,5 +43,10 @@ public class TestController {
     @GetMapping("/tasks")
     public List<Task> getTasks() {
         return taskService.findAll();
+    }
+
+    @GetMapping("/companies")
+    public List<Company> getCompanies() {
+        return companyRepository.findAll();
     }
 }
